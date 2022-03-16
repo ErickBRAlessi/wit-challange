6454636d3525dfb507d61edd85caed6a53d3b0ef
ErickBRAlessi <erick_alessi@outlook.com>
2022-03-16 19:17:33 -0300

    modules created

---
+++ calculator/src/main/java/br/alessi/calculator/CalculatorApplication.java
@@ -0,0 +1,15 @@
+package br.alessi.calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorApplication.class, args);
+    }
+
+
+}
